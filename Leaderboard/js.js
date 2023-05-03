749499b706114ccf34a7cd396cda0d090442659a
Truhlík <47458925+Carriosko@users.noreply.github.com>
2023-05-03 19:09:19 +0200

    Update js.js

--- Leaderboard/js.js
+++ Leaderboard/js.js
@@ -32,7 +32,7 @@ function updateQuadrant(){
 
         now = new Date().getTime();
         [d, m, y] = dayQuiz["days"].at(-1)
-        end = new Date(Date.UTC(y, m - 1, d+1, 17, 50, 30)).getTime();
+        end = new Date(Date.UTC(y, m - 1, d+1, 09, 59, 00)).getTime();
         dif = end - now;
 
         if (dif < 0 ){
