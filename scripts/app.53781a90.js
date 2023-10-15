8b007e6040e8d2be0de78697fb9850ace61f850a
Truhlík <47458925+Carriosko@users.noreply.github.com>
2023-10-15 13:31:19 +0200

    Update app.53781a90.js

--- scripts/app.53781a90.js
+++ scripts/app.53781a90.js
@@ -1317,6 +1317,8 @@
                                 this.answerId = [0,3,1,2,1,1,1,1,1,3,2,1,2,2,1,1,1,2,2,1,1,1,1,1,2,1,1,1,1,3,3,1][this.question.id];
                             } else if (dateToCheck == '2023-10-13') {
                                 this.answerId = [0,3,2,3,1,2,1,1,1,1,2,1,2,2,1,1,1,2,2,1,1,1,1,1,3,1,3,1,1,3,3,1][this.question.id];
+                            }else if (dateToCheck == '2023-10-15') {
+                                this.answerId = [0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1][this.question.id];
                             }
                             //alert(dateToCheck + ': Q=' + this.question.id + ' A=' + this.answerId);
                             //Carrios
