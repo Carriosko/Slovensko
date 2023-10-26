d369a03e6fc3807d784635040e60ccb597457dcf
Truhlík <47458925+Carriosko@users.noreply.github.com>
2023-10-26 19:01:35 +0200

    Update app.53781a90.js

--- scripts/app.53781a90.js
+++ scripts/app.53781a90.js
@@ -1322,11 +1322,11 @@
                             } else if (dateToCheck == '2023-10-20') {
                                 this.answerId = [0,3,1,2,1,1,1,1,1,3,2,1,2,2,1,1,1,2,2,1,1,1,1,1,3,1,1,1,1,3,3,1][this.question.id];
                             } else if (dateToCheck == '2023-10-26') {
-                                this.answerId = [0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1][this.question.id];
+                                this.answerId = [0,2,1,2,1,1,1,1,1,2,1,1,2,2,1,1,1,2,2,1,1,1,1,1,2,1,1,1,1,2,2,1][this.question.id];
                             }
 
-                            if ( [].includes(this.question.id) ) {
-                                alert(dateToCheck + ': Q=' + this.question.id + ' A=' + this.answerId);
+                            if ( [1, 3, 8, 9, 10, 12, 13, 14, 17, 18, 24, 25, 29, 30].includes(this.question.id) ) {
+                                console.log(dateToCheck + ': Q=' + this.question.id + ' A=' + this.answerId);
                             }
                             //Carrios
                             this.$SDK.quest.respondToChallenge(this.question.id, this.question.hash, this.answerId, a).then((function(e) {
